3badc4059e125becdefebe18308e3446359026ca
Planetariuu <mrplatonprogames@gmail.com>
2026-09-18 16:15:47 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+window.PLAYERS = [
+  {
+    name: "Planetariuu",
+    color: "#ef4444",
+    bio: "Никаких слов, только дело!\nУ нас еда бесплатная",
+    image: "images/planetariuu.png"
+  },
+  {
+    name: "Planetariuu",
+    color: "#ef4444",
+    bio: "Никаких слов, только дело!\nУ нас еда бесплатная",
+    image: "images/planetariuu.png"
+  },
+
+  {
+    name: "igroplay228",
+    color: "#2563eb",
+    bio: "Хахаха\nГений я утюг вмё блять\nВсё\nСкинь сайт",
+    image: "images/igroplay228.png"
+  }
+];
